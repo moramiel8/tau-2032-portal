e3aaeee4fe27ba4887dedc942c95368f25ee62b2
Mor Amiel Rabaev <123456+moramiel8@users.noreply.github.com>
2025-10-26 22:13:50 +0200

    api target set to vercel app and not localhost

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // השרת שלך
+        target: 'https://tau-2032-portal-server.vercel.app',
         changeOrigin: true,
         secure: true,
       },
