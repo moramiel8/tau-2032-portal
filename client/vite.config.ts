997156c1f30c540709b097302ff2f9461c519a03
Mor Amiel Rabaev <morku8@gmail.com>
2025-11-06 07:48:46 +0200

    fix retriving client/dist and not just dist in vercel json file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/client/', // ← חשוב!
+  base: '/client/',
   plugins: [react()],
 })
