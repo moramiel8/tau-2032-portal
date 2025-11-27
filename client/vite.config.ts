52be4212f944761e3248727ec87f788ec67b1745
Mor Amiel Rabaev <morku8@gmail.com>
2025-11-27 10:56:50 +0200

    update vercel json file to valid one

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,8 @@
 // client/vite.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// vite.config.ts
 export default defineConfig({
-  base: '/',         // or correct base if app is under /client
-  build: {
-    outDir: 'dist',  // and deploy the contents of dist directly
-  },
+  base: '/',      // important: so assets are /assets/...
+  build: { outDir: 'dist' }
 });
+
