673cb67715cf361dfbd2206ffca912b815432cd2
Mor Amiel Rabaev <morku8@gmail.com>
2025-12-30 15:37:01 +0200

    update main tsx with vercel speed insights

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,8 +6,7 @@ import App from "./App";
 import "./index.css";
 
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react"
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
